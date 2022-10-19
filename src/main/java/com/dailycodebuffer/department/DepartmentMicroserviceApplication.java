8d7df38996c0961b4724a161bb93f89fcae4d52f
toluwase <toluwasethomas1@gmail.com>
2022-10-19 14:41:32 +0100

    departmental microservice

---
+++ src/main/java/com/dailycodebuffer/department/DepartmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentMicroserviceApplication.class, args);
+	}
+
+}
